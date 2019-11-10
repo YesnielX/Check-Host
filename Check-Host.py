e234920cbd0be22fbe0f00675202c2208c781394
YesnielX <35044004+jojosamass@users.noreply.github.com>
2019-11-10 15:26:28 -0400

    Update Check-Host.py

--- Check-Host.py
+++ Check-Host.py
@@ -1,37 +1,40 @@
 #Check host
-import socket # import socket
-from os import sys, system  
+from os import sys, system
+import os  
 import time
 
 
 #def check-host
 def check_host():
-  # default port for socket 
-  port = 80
   host = input('Enter de Host: ')
   print('')
-  try: 
-    host_ip = socket.gethostbyname(host) 
-  except socket.gaierror: 
-  
-    # this means could not resolve the host 
-    print ("There was an error verifying the host")
-    sys.exit() 
-    print('')
-  #  connecting to the server 
-  s.connect((host_ip, port)) 
-
-  
-  print ("the host is working perfectly, connected to on port %s" %(host_ip) )
-  time.sleep(3)
-  print('')
-  backmenu = input('Do you want to return to the start menu? [y/n].')
+  response = os.system("ping -c 1 " + host + " > /dev/null 2>&1")
+  if response == 0:
+    print('Waiting host . . . ')
+    time.sleep(3)
+    print ("%s The host works perfectly" % host)
+    time.sleep(3)
+    backmenu = input('Do you want to return to the start menu? [y/n].')
+  else:
+    print ("%s The host does not work (Does not respond)" % host)
+    
   if backmenu == 'y':
     system('clear')
+    system('cls')
     options()
     input('Check-Host>')
     selected()
   else:
+    system('clear')
+    system('cls')
+    print('''
+  ======================================
+   THANKS for using Check Host
+
+      follow on github @jojosamass
+  ======================================
+    ''')
+    time.sleep(5)
     system('clear')
     sys.exit()
 
@@ -60,7 +63,6 @@ options() #print options
 
 input_user = input('Check Host> ')
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 
 def selected():
   #option 1
@@ -77,20 +79,24 @@ def selected():
     system('clear')
     print(check_host())
 
-  else:
-    sys.exit()
 
 #option 2
   if input_user == '2': 
     system('clear')
     system('git clone https://github.com/jojosamass/Check-host')
 
-  else:
-    sys.exit()
-
-#option 1
+#option 3
   if input_user == '3': 
+    print('''
+    ======================================
+    THANKS for using Check Host
+
+      follow on github @jojosamass
+    ======================================
+    ''')
+    time.sleep(5)
     system('clear')
+    system('cls')
     sys.exit()
     
 selected()
